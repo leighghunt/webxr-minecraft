81766a965be3b5db44c90ce7ac193f36fabc1740
Glitch (aframe) <none>
2021-07-23 05:03:34 +0000

    👔🚐 Checkpoint ./index.html:72439/538 ./client.js:72439/1095

---
+++ client.js
@@ -0,0 +1,20 @@
+var sphere = document.getElementById('sphere');
+
+var cylinder = document.getElementById('cylinder');
+
+// console.log("el....")
+// console.log(el)
+
+setInterval(function(){
+  var positionSphere = sphere.getAttribute('position')
+  positionSphere.y+=0.005
+  sphere.setAttribute('position', positionSphere);
+  
+
+  var positionCylinder = cylinder.getAttribute('position')
+  positionCylinder.x-=0.005
+  cylinder.setAttribute('position', positionCylinder);
+
+
+}, 50)
+//0 1.25 -5
