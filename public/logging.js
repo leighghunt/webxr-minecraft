340a6b4cdfb06dcc68fbf8a7b678f05a7c77d3b8
Glitch (webxr-mc) <none>
2021-11-13 10:53:20 +0000

    ❣️🌺 Checkpoint ./public/logging.js:72439/1462

--- public/logging.js
+++ public/logging.js
@@ -11,74 +11,75 @@ AFRAME.registerComponent('thumbstick-logging',{
     this.el.addEventListener('thumbstickmoved', this.logThumbstick);
   },
   logThumbstick: function (evt) {
-    log('logThumbstick')
-  alert('logThumbstick')
+    // log('logThumbstick')
+  // alert('logThumbstick')
 
     //log('evt')
     //log(evt)
 
     //log('evt.detail')
     //log(evt.detail)
+    // alert(evt.detail)
     
     var player = document.querySelector("a-entity[camera]")
 
 
     
-      // log("position")
-      // log(player.getAttribute("position"))
+//       // log("position")
+//       // log(player.getAttribute("position"))
 
-      var angle = player.getAttribute("rotation")
-      // log("angle")
-      // log(angle)
-
-
-          // calculate the angles
-          // the camera's theta == 0 is actually 90' in the clipspace
-          let theta = (angle.x * Math.PI / 180) + Math.PI / 2 
-          let fi = angle.y * Math.PI / 180
-          let r = 10
-          // calculate the position shifts
-          let z = Math.sin(theta) * Math.cos(fi) * r
-          let x = Math.sin(theta) * Math.sin(fi) * r
-          let y = Math.cos(theta) * r
-
-          // update the position
-          var pos = player.getAttribute("position")
-          var newPos = {x: pos.x,
-                       y: pos.y,
-                       z: pos.z}
-//           log(pos)
-//     log(x)
-
-//     log(y)
-
-//     log(z)
-
-          player.object3D.position.x -= x;
-          player.object3D.position.y -= y;
-          player.object3D.position.z -= z;
-    
-    // alert(newPos);
-
-          // newPos.x -= x;
-          // newPos.y -= y;
-          // newPos.z -= z;
-    
-          // player.object3D.position.set(newPos.x, newPos.y, newPos.z);
-    
-          // log("Moving")
-          // log(newPos)
-          // player.setAttribute('position', newPos);
-    
-    
-    if (evt.detail.y > 0.95) { 
-
+//       var angle = player.getAttribute("rotation")
+//       // log("angle")
+//       // log(angle)
       
-          log("DOWN"); }
+//       // alert(evt.detail.y)
+//       // alert(angle)
+
+
+//           // calculate the angles
+//           // the camera's theta == 0 is actually 90' in the clipspace
+//           let theta = (angle.x * Math.PI / 180) + Math.PI / 2 
+//           let fi = angle.y * Math.PI / 180
+//           let r = 10
+//           // calculate the position shifts
+//           let z = Math.sin(theta) * Math.cos(fi) * r
+//           let x = Math.sin(theta) * Math.sin(fi) * r
+//           let y = Math.cos(theta) * r
+
+//           // update the position
+//           var pos = player.getAttribute("position")
+//           var newPos = {x: pos.x,
+//                        y: pos.y,
+//                        z: pos.z}
+
+//           player.object3D.position.x -= x;
+//           player.object3D.position.y -= y;
+//           player.object3D.position.z -= z;
     
-    if (evt.detail.y < -0.95) { log("UP"); }
-    if (evt.detail.x < -0.95) { log("LEFT"); }
-    if (evt.detail.x > 0.95) { log("RIGHT"); }
+    
+    
+    if (evt.detail.y > 0.95) {   
+        player.object3D.position.y += evt.detail.y
+        log("DOWN"); }
+    
+    if (evt.detail.y < -0.95) { 
+        player.object3D.position.y += evt.detail.y
+      log("UP"); 
+    }
+    if (evt.detail.x < -0.95) { 
+        player.object3D.position.x += evt.detail.x
+      log("LEFT"); 
+    }
+    if (evt.detail.x > 0.95) { 
+      log('player.object3D.position.x')
+      log(player.object3D.position.x)
+      log('player.getAttribute("position").x')
+      log(player.getAttribute("position").x)
+      player.object3D.position.x += evt.detail.x
+      log(player.object3D.position.x)
+      log(player.getAttribute("position").x)
+      log("RIGHT"); 
+    }
     
   }
 });
